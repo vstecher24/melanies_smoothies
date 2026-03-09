6df9d81cd351f7e7dc1da93143ceaf9aec00c75b
vstecher24 <160566827+vstecher24@users.noreply.github.com>
2026-03-09 19:32:28 +1100

    Modify smoothie options to include SEARCH_ON
    Updated the data selection to include 'SEARCH_ON' and added a dataframe display. Stopped execution after displaying the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col 
 import requests  
-
+import pandas as pd
 
 
 # Write directly to the app
@@ -16,12 +16,13 @@ st.write(
 # Get the current credentials
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:', name_on_order)
+
 
 
 ingredients_list = st.multiselect(
@@ -46,7 +47,6 @@ if ingredients_list:
             values ('""" + ingredients_string   + """' , '""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
-    #st.stop()
     time_to_insert = st.button('Submit Order')
 
         
