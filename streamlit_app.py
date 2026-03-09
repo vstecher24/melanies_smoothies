f38dbfe2d306d8e60b38695098e86f0e1b2f4e24
vstecher24 <160566827+vstecher24@users.noreply.github.com>
2026-03-09 19:18:29 +1100

    Change smoothie response display to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,8 @@ if ingredients_list:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!',  icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/elderberry")  
-st.text(smoothiefroot_response.json())
-
+#.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
    
 
